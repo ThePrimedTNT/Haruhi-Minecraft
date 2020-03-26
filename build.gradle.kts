ab0845027638f004bc9385d97a86a49ccd9c3a5f
KJP12 <kjp12.k.joy.programer12@gmail.com>
2020-03-25 17:15:09 -0700

    Kotlin DSL

---
+++ build.gradle.kts
@@ -0,0 +1,49 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    java
+    kotlin("jvm") version "1.3.71"
+    kotlin("plugin.serialization") version "1.3.71"
+}
+
+allprojects {
+    apply(plugin = "java")
+    apply(plugin = "org.jetbrains.kotlin.jvm")
+    apply(plugin = "org.jetbrains.kotlin.plugin.serialization")
+
+    group = "ai.haruhi"
+    version = "0.0.0"
+
+    repositories {
+        mavenCentral()
+        jcenter()
+    }
+
+    dependencies {
+        implementation(kotlin("stdlib-jdk8"))
+    }
+
+    configure<JavaPluginConvention> {
+        sourceCompatibility = JavaVersion.VERSION_13
+        targetCompatibility = JavaVersion.VERSION_13
+    }
+
+    tasks {
+        withType<JavaCompile> {
+            options.isDeprecation = true
+            options.isFork = true
+            options.encoding = "UTF-8"
+            options.isVerbose = true
+        }
+        withType<KotlinCompile> {
+            kotlinOptions.jvmTarget = "13"
+        }
+        /*val shadowJar = getByName<ShadowJar>("shadowJar") {
+            archiveVersion.set(project.version.toString())
+            manifest.attributes["Implementation-Version"] = project.version.toString()
+        }
+        "build" {
+            dependsOn(shadowJar)
+        }*/
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,3 @@
+rootProject.name = "Haruhi-Minecraft"
+
+include("core")
